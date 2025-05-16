23b759ac57c22fdcb4d0e9b95c6ef30eba12cfff
JennyGD1 <jenniferlima61@gmail.com>
2025-05-16 16:25:39 -0300

    Update gunicorn.conf.py

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,6 +1,6 @@
 # gunicorn.conf.py
-bind = "0.0.0.0:8080"  # Porta padrão do Render
-workers = 2          # Número de workers
-timeout = 120        # Timeout em segundos (aumentado)
-keepalive = 5          # Keep-alive em segundos
-worker_class = "sync"  # Tipo de worker
+bind = "0.0.0.0:8080"
+workers = 2
+timeout = 180  # Aumentando o timeout do Gunicorn
+keepalive = 5
+worker_class = "sync"
